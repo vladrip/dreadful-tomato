44951271fcdee87264f3ae31d0f7433482a9236e
vladrip <vladrip12@gmail.com>
2024-02-13 18:52:17 +0200

    create landing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from "./components/layout/layout.component";
+import { LandingComponent } from "./components/common/landing/landing.component";
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
     children: [
-
+      { path: '', component: LandingComponent }
     ]
   }
 ]
